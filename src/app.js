4fc627bea1dcae51109c25d522f1adce11208ab0
Valentino Nathan <nathan070907@gmail.com>
2026-06-08 20:54:32 +0700

    connect to db

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from "express";
+import db from "./db/index.js";
+
 const app = express();
 const developmentPort = 4000;
 
 app.use(express.json());
 
-app.get("/", (req, res, next) => {
-    res.status(200).send("Welcome to NUSphere Server");
-})
+app.get("/", async (req, res, next) => {
+    try {
+        let test = await db.query("SELECT * FROM test");
+        test = test?.rows;
+        res.status(200).send(test);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
 app.listen(developmentPort, () => {console.log(`NUSphere server listening on port ${developmentPort}`)});
