65001e6903d0cb27310472e4ea15569a07ff0533
Valentino Nathan <nathan070907@gmail.com>
2026-06-10 15:59:00 +0700

    deployment fix

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://nusphere-seven.vercel.app/"
+    "https://nusphere-seven.vercel.app"
 ];
 
 app.use(cors({
