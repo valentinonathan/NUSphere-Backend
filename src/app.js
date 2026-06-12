9114a31b03836d27fba7252de9684ae0405db19c
Valentino Nathan <nathan070907@gmail.com>
2026-06-13 01:34:45 +0700

    fixed data type

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.get("/", authenticateRequest, async (req, res, next) => {
     try {
         let test = await db.query("SELECT * FROM test");
         test = test?.rows;
-        res.status(200).send(test);
+        res.status(200).json({message: test});
     } catch (error) {
         res.status(500).send(error.message);
     }
