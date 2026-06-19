bb5479f30d2603affd442e05dc518169b903c726
Valentino Nathan <nathan070907@gmail.com>
2026-06-19 17:28:51 +0700

    Forgotten fix

--- src/routes/friendRequests.route.js
+++ src/routes/friendRequests.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { authenticateRequest } from "../middleware/auth.middleware.js";
 import { friendRequestController, friendRequestStatusController } from "../controllers/friendRequests.controllers.js";
+import { friendRequestValidator } from "../middleware/friendRequests.middleware.js";
 const friendRequestsRouter = express.Router();
 
-friendRequestsRouter.post("/:receiverId", authenticateRequest, friendRequestController);
+friendRequestsRouter.post("/:receiverId", authenticateRequest, friendRequestValidator, friendRequestController);
 friendRequestsRouter.get("/:receiverId", authenticateRequest, friendRequestStatusController);
 
 export { friendRequestsRouter }
