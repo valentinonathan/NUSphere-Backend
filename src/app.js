bcf07c52d26deaf22236920e07a5cb40d6e63036
CAYOPAN <calvinyopan07@gmail.com>
2026-07-26 23:48:23 +0800

    deploy fix

--- src/app.js
+++ src/app.js
@@ -44,6 +44,8 @@ import { commentRouter } from "./routes/comment.route.js";
 import { friendRequestsRouter } from "./routes/friendRequests.route.js";
 import { eventAttendanceRouter } from "./routes/event.attendance.route.js";
 import { conversationsRouter } from "./routes/conversation.route.js";
+import { moduleRouter } from "./routes/module.route.js";
+import { marketRouter } from "./routes/market.route.js";
 
 app.use("/auth", authRouter);
 app.use("/events", eventRouter);
@@ -53,6 +55,8 @@ app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 app.use("/friend-requests", friendRequestsRouter);
 app.use("/conversations", conversationsRouter);
+app.use("/market", marketRouter);
+app.use("modules", moduleRouter)
 
 import { authenticateRequest } from "./middleware/auth.middleware.js";
 import { getUserDetailsByUserId } from "./utils/user.utils.js";
